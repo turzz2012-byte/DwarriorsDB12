ff33943ab910aa778b5729f0a247bcc5a981a1ca
turzz2012-byte <turzz2012@gmail.com>
2026-06-14 20:57:01 +0700

    Update tokens - 2026-06-14T13:57:01.197Z

--- tokens.js
+++ tokens.js
@@ -3,6 +3,7 @@
     "Token lu",
     "8777288730:AAF7kRoUuXSFVVfnZfR2j8_nPahMklKcfYw",
     "8109838845:AAHMQ1-QzieniRg_LBp_6CAeAcI-CZtocyw",
-    "8940391447:AAGzgTRsQKxtXZ8GVd_8YgVu_qk6LQBLZ4M"
+    "8940391447:AAGzgTRsQKxtXZ8GVd_8YgVu_qk6LQBLZ4M",
+    "8968033533:AAESqanFGlDcReRrReHqN80lczp0oTeh-HQ"
   ]
 }
