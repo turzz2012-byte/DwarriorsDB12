9f7d43985469bd2c188991d1ac7e87bf1cb3aea0
turzz2012-byte <turzz2012@gmail.com>
2026-06-14 11:05:08 +0700

    Update tokens - 2026-06-14T04:05:08.268Z

--- tokens.js
+++ tokens.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "Token lu",
-    "8777288730:AAF7kRoUuXSFVVfnZfR2j8_nPahMklKcfYw"
+    "8777288730:AAF7kRoUuXSFVVfnZfR2j8_nPahMklKcfYw",
+    "8109838845:AAHMQ1-QzieniRg_LBp_6CAeAcI-CZtocyw"
   ]
 }
